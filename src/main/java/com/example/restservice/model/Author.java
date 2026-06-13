2af24862b8fd0af42e71205ca8e9c80e87f9582e
Benjamin Alexander <wetbadger49@gmail.com>
2026-06-13 14:32:02 -0400

    jwt service

--- src/main/java/com/example/restservice/model/Author.java
+++ src/main/java/com/example/restservice/model/Author.java
@@ -1,4 +1,78 @@
 package com.example.restservice.model;
 
-public class Author {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+public class Author implements UserDetails {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 30)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private boolean enabled;
+    private String verificationCode;
+    private LocalDateTime verificationCodeExpired;
+
+    public Author(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public Author() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
---
+++ src/main/java/com/example/restservice/repository/AuthorRepository.java
@@ -0,0 +1,13 @@
+package com.example.restservice.repository;
+
+import com.example.restservice.model.Author;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+    Optional<Author> findAuthorByUsername(String username);
+    Optional<Author> findAuthorByVerificationCode(String verificationCode);
+}
