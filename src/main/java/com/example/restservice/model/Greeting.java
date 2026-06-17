bcc731cf9bcf2b8e10f0fb8eb5cde59ebeeb4b4a
Benjamin Alexander <3209628@tcs.com>
2026-06-17 14:07:24 -0400

    update readme

--- src/main/java/com/example/restservice/model/Greeting.java
+++ src/main/java/com/example/restservice/model/Greeting.java
@@ -1,3 +1,8 @@
+/**
+ * You can go to localhost:8080/greeting to test the api
+ * It should say 403 with Spring Security working
+ */
+
 package com.example.restservice.model;
 
 public record Greeting(long id, String content) { }
